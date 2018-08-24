54f247ecec9b520a13990b1645b9bf5b0526b118
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2018-08-24 17:14:22 -0400

    Created base NomadApi and job api...
    Still trying to creat tests though.

---
+++ Nomad.DotNet/Nomad.DotNet/API/JobApi.cs
@@ -0,0 +1,18 @@
+﻿using Nomad.DotNet.Model;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Nomad.DotNet.API
+{
+    public class JobApi : NomadApi<Job>
+    {
+        public JobApi(HttpClient httpClient) : base(httpClient)
+        {
+        }
+
+        protected override string resourceName => "job";
+
+    }
+}
---
+++ Nomad.DotNet/Nomad.DotNet/API/NomadApi.cs
@@ -0,0 +1,42 @@
+﻿using Nomad.DotNet.Model;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nomad.DotNet.API
+{
+    public abstract class NomadApi<T> where T : ApiObject<T>
+    {
+        private string apiVersion = "v1";
+        protected abstract string resourceName { get; }
+
+        HttpClient httpClient;
+        UriBuilder builder;
+        
+        public NomadApi(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            builder = new UriBuilder("http", "127.0.0.1", 4646);
+        }
+
+        protected Uri buildUriForResourceId(string id)
+        {
+            Uri relativeUri = new Uri($"{apiVersion}/{resourceName}/{id}", UriKind.Relative);
+            Uri fullUri = new Uri(builder.Uri, relativeUri);
+
+            return fullUri;
+        }
+
+        public async Task<T> GetAsync(string id)
+        {
+            Uri uri = buildUriForResourceId(id);
+
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            T resource = await response.Content.ReadAsAsync<T>();
+
+            return resource;
+        }
+    }
+}
