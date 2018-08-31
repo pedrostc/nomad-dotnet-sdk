2087b48ecbe55edad20a8d0b6e971bedc3d983ec
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2018-08-31 09:13:40 -0400

    Create Stub for NomadApi base class for tests

---
+++ src/Nomad.DotNet.Tests/API/NomadApiStub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Nomad.DotNet.API;
+
+namespace Nomad.DotNet.Tests.API
+{
+    public class NomadApiStub : NomadApi
+    {
+        public NomadApiStub(HttpClient httpClient, NomadApiConfig apiConfig) : base(httpClient, apiConfig)
+        { }
+
+        protected override string resourceName => "path";
+
+        protected override string collectionName => "path";
+
+        public async Task<dynamic> GetWithType()
+        {
+            Uri uri = buildResourceUri();
+            dynamic response = await ProcessGetAsync<dynamic>(uri);
+
+            return response;
+        }
+
+        public async Task<string> GetText()
+        {
+            Uri uri = buildResourceUri();
+            string response = await ProcessGetTextAsync(uri);
+
+            return response;
+        }
+    }
+}
---
+++ src/Nomad.DotNet.Tests/API/NomadApiTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nomad.DotNet.API;
+using RichardSzalay.MockHttp;
+using System;
+
+namespace Nomad.DotNet.Tests.API
+{
+    [TestClass]
+    public class NomadApiTests
+    {
+        NomadApiConfig apiConfig = new NomadApiConfig
+        {
+            HostUri = "http://127.0.0.1:4646"
+        };
+
+        [TestMethod]
+        public void GetWithType_ValidRequest_ObjectInstance()
+        {
+            string fileContent = @"{ key: ""value"" }";
+            Uri jobUri = new Uri("http://127.0.0.1:4646/v1/path");
+
+            MockHttpMessageHandler mockHttp = new MockHttpMessageHandler();
+            var expectedRequest = mockHttp.When(jobUri.AbsoluteUri)
+                    .Respond("application/json", fileContent);
+
+            NomadApiStub api = new NomadApiStub(mockHttp.ToHttpClient(), apiConfig);
+
+            dynamic response = api.GetWithType().GetAwaiter().GetResult();
+
+            Assert.IsNotNull(response);
+        }
+    }
+}
