7094cf14716db7d147fc2b0e3d4d2c72aa57cd9b
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2018-08-31 15:05:19 -0400

    Added Search API

---
+++ src/Nomad.DotNet/API/Search.cs
@@ -0,0 +1,29 @@
+﻿using Nomad.DotNet.Model;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Nomad.DotNet.API
+{
+    public class Search : NomadApi
+    {
+        public Search(HttpClient httpClient, NomadApiConfig apiConfig) :
+            base(httpClient, apiConfig)
+        {
+        }
+
+        protected override string resourceName => "search";
+
+        protected override string collectionName => throw new NotImplementedException();
+
+        public async Task<SearchResponse> PerformSearch(string prefix, string context)
+        {
+            var requestObj = new { Prefix = prefix, Context = context };
+            Uri uri = buildResourceUri();
+
+            SearchResponse response = await ProcessPostAsync<SearchResponse>(uri, requestObj);
+
+            return response;
+        }
+    }
+}
