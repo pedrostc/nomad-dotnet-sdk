12d00d1c3f18e30c4a78bf581a1a535fd8607540
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2018-08-31 09:13:22 -0400

    Added Client Api and some tests

---
+++ src/Nomad.DotNet.Tests/API/ClientTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nomad.DotNet.API;
+using RichardSzalay.MockHttp;
+using System;
+
+namespace Nomad.DotNet.Tests.API
+{
+    [TestClass]
+    public class ClientTests
+    {
+        NomadApiConfig apiConfig = new NomadApiConfig
+        {
+            HostUri = "http://127.0.0.1:4646"
+        };
+
+        [TestMethod]
+        public void ReadFile_ValidRequest_FileContent()
+        {
+            string allocationId = "123456";
+            string fileContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            Uri jobUri = new Uri("http://127.0.0.1:4646/v1/client/fs/cat/123456");
+
+            MockHttpMessageHandler mockHttp = new MockHttpMessageHandler();
+            var expectedRequest = mockHttp.When(jobUri.AbsoluteUri)
+                    .Respond("text/plain", fileContent);
+
+            Client api = new Client(mockHttp.ToHttpClient(), apiConfig);
+
+            string response = api.ReadFile(allocationId).GetAwaiter().GetResult();
+
+            Assert.AreEqual(fileContent, response);
+        }
+    }
+}
---
+++ src/Nomad.DotNet/API/Client.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Nomad.DotNet.Model;
+using Nomad.DotNet.UriUtilities;
+
+namespace Nomad.DotNet.API
+{
+    public class Client : NomadApi
+    {
+        private const string PATH_QUERY_FIELD = "path";
+        private const string OFFSET_QUERY_FIELD = "offset";
+        private const string LIMIT_QUERY_FIELD = "limit";
+        
+        private const string FILE_SYSTEM_PATH = "fs";
+        private const string ALLOCATION_PATH = "allocation";
+
+        public Client(HttpClient httpClient, NomadApiConfig apiConfig) :
+            base(httpClient, apiConfig)
+        {
+        }
+
+        protected override string resourceName => "client";
+
+        protected override string collectionName => throw new NotImplementedException();
+
+        public async Task<HostStats> Stats()
+        {
+            Uri uri = buildResourceUri(method: "stats");
+            HostStats stats = await ProcessGetAsync<HostStats>(uri);
+
+            return stats;
+        }
+
+
+        private Uri buildAllocationUri(string method, string allocationId)
+        {
+            Uri baseUri = buildResourceUri();
+            BetterUriBuilder builder = new BetterUriBuilder(baseUri);
+            builder.AddPathPart(ALLOCATION_PATH);
+            builder.AddPathPart(allocationId);
+            builder.AddPathPart(method);
+
+            return builder.Uri;
+        }
+
+        public async Task<HostStats> AllocationStats(string allocationId)
+        {
+            
+            Uri uri = buildAllocationUri("stats", allocationId);
+            HostStats stats = await ProcessGetAsync<HostStats>(uri);
+
+            return stats;
+        }
+
+        private Uri buildFileSystemUri(
+            string method,
+            string allocationId,
+            string path)
+        {
+            Uri baseUri = buildResourceUri();
+            BetterUriBuilder builder = new BetterUriBuilder(baseUri);
+
+            builder.AddPathPart(FILE_SYSTEM_PATH);
+            builder.AddPathPart(method);
+            builder.AddPathPart(allocationId);
+
+            builder.AddQueryField(PATH_QUERY_FIELD, path);
+
+            return builder.Uri;
+        }
+        public async Task<string> ReadFile(string allocationId, string path)
+        {
+            Uri uri = buildFileSystemUri("cat", allocationId, path);
+            string fileContent = await ProcessGetTextAsync(uri);
+
+            return fileContent;
+        }
+
+        private Uri buildReadFileAtOffsetUri(
+            string allocationId,
+            string path,
+            int offset,
+            int limit)
+        {
+            Uri baseUri = buildResourceUri();
+            BetterUriBuilder builder = new BetterUriBuilder(baseUri);
+
+            builder.AddPathPart(FILE_SYSTEM_PATH);
+            builder.AddPathPart("readat");
+            builder.AddPathPart(allocationId);
+
+            builder.AddQueryField(PATH_QUERY_FIELD, path);
+            builder.AddQueryField(OFFSET_QUERY_FIELD, offset.ToString());
+            builder.AddQueryField(LIMIT_QUERY_FIELD, limit.ToString());
+
+            return builder.Uri;
+        }
+        public async Task<string> ReadFileAtOffset(
+            string allocationId,
+            string path,
+            int offset,
+            int limit)
+        {
+            
+            Uri uri = buildReadFileAtOffsetUri(allocationId, path, offset, limit);
+            string fileContent = await ProcessGetTextAsync(uri);
+
+            return fileContent;
+        }
+
+        public async Task<IList<AllocFileInfo>> ListFiles(
+            string allocationId,
+            string path)
+        {
+
+            Uri uri = buildFileSystemUri("ls", allocationId, path);
+            IList<AllocFileInfo> files = await ProcessGetAsync<List<AllocFileInfo>>(uri);
+
+            return files;
+        }
+        public async Task<IList<AllocFileInfo>> StatFile(
+            string allocationId,
+            string path)
+        {
+
+            Uri uri = buildFileSystemUri("stat", allocationId, path);
+            IList<AllocFileInfo> files = await ProcessGetAsync<List<AllocFileInfo>>(uri);
+
+            return files;
+        }
+
+        public async System.Threading.Tasks.Task GCAllocation(string allocationId)
+        {
+            Uri uri = buildAllocationUri("gc", allocationId);
+            await SendGetAsync(uri);
+        }
+        public async System.Threading.Tasks.Task GCAllAllocations(string allocationId)
+        {
+            Uri uri = buildResourceUri(method: "gc");
+            await SendGetAsync(uri);
+        }
+
+        //TODO: Implement STREAM methods.
+    }
+}
