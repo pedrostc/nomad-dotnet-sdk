6aa3792bebbc1ffdc3a899ec9046a4c6230b2be2
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2018-08-31 15:51:27 -0400

    Added ValidateApi

---
+++ src/Nomad.DotNet/API/Validate.cs
@@ -0,0 +1,28 @@
+﻿using Nomad.DotNet.Model;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Nomad.DotNet.API
+{
+    public class Validate : NomadApi
+    {
+        public Validate(HttpClient httpClient, NomadApiConfig apiConfig) :
+            base(httpClient, apiConfig)
+        {
+        }
+
+        protected override string resourceName => "validate";
+
+        protected override string collectionName => throw new NotImplementedException();
+
+        public async Task<JobValidateResponse> Job(Job job)
+        {
+            Uri uri = buildResourceUri(method: "job");
+            JobValidateResponse response = 
+                await ProcessPostAsync<JobValidateResponse>(uri, job);
+
+            return response;
+        }
+    }
+}
