e8c8c979065f9d6344baa031722f473c66c9f513
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2018-08-31 14:49:21 -0400

    Added Regions Api

---
+++ src/Nomad.DotNet/API/Regions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Nomad.DotNet.API
+{
+    public class Regions : NomadApi
+    {
+        public Regions(HttpClient httpClient, NomadApiConfig apiConfig) :
+            base(httpClient, apiConfig)
+        {
+        }
+
+        protected override string resourceName => throw new NotImplementedException();
+
+        protected override string collectionName => "regions";
+
+        public async Task<IList<string>> List()
+        {
+            Uri uri = buildCollectionUri();
+            IList<string> regions = await ProcessGetAsync<List<string>>(uri);
+
+            return regions;
+        }
+    }
+}
