bd4a8d45fc69bd4920bb12dc1a2e1eb1a1ab04c9
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2018-09-05 16:54:26 -0400

    Added ServiceCollectionExtensions for injecting all the API objects into the DI container.

---
+++ src/Nomad.DotNet/ServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Nomad.DotNet.API;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddBackgroundTasksQueue(
+            this IServiceCollection services)
+        {
+            services.AddScoped<AclPolicies>();
+            services.AddScoped<AclTokens>();
+            services.AddScoped<Agent>();
+            services.AddScoped<Allocations>();
+            services.AddScoped<Client>();
+            services.AddScoped<Deployments>();
+            services.AddScoped<Evaluations>();
+            services.AddScoped<Jobs>();
+            services.AddScoped<Namespaces>();
+            services.AddScoped<Nodes>();
+            services.AddScoped<Quotas>();
+            services.AddScoped<Regions>();
+            services.AddScoped<Search>();
+            services.AddScoped<SentinelPolicies>();
+            services.AddScoped<Status>();
+            services.AddScoped<SystemMaintenance>();
+            services.AddScoped<Validate>();
+
+            return services;
+        }
+    }
+}
