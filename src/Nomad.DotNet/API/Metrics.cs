e7aaa888835a6f4060d881437102014cf271ab03
Pedro Osternack Corrêa <pedrost.rj@gmail.com>
2019-02-06 12:55:51 -0500

    Created the MetricsApi wrapper with its get method.

---
+++ src/Nomad.DotNet/API/Metrics.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Nomad.DotNet.UriUtilities;
+
+namespace Nomad.DotNet.API
+{
+    public class Metrics : NomadApi
+    {
+        public const string FORMAT_QUERY_FIELD = "format";
+
+        public Metrics(HttpClient httpClient, NomadApiConfig apiConfig)
+            : base(httpClient, apiConfig)
+        {
+        }
+
+        protected override string resourceName => "metrics";
+
+        protected override string collectionName => throw new NotImplementedException();
+
+        private Uri buildMetricsUri(MetricsFormat format)
+        {
+            Uri uri = buildResourceUri();
+            BetterUriBuilder builder = new BetterUriBuilder(uri);
+
+            builder.AddQueryField(FORMAT_QUERY_FIELD, format.ToString());
+
+            return builder.Uri;
+        }
+        public async Task<string> GetMetrics(MetricsFormat format = MetricsFormat.JSON)
+        {
+            Uri uri = buildMetricsUri(format);
+            string metrics = await ProcessGetTextAsync(uri);
+
+            return metrics;
+        }
+    }
+
+    public enum MetricsFormat
+    {
+        JSON,
+        prometheus
+    }
+}
